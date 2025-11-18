7858de45444a24fd3d45d779117a13caa1afff71
Lana Dzyuban <svitlana.dzyuban@gmail.com>
2025-11-18 16:53:56 -0500

    Primitive types and vectors

--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -3,7 +3,7 @@ fn vec_loop(input: &[i32]) -> Vec<i32> {
 
     for element in input {
         // TODO: Multiply each element in the `input` slice by 2 and push it to
-        // the `output` vector.
+        output.push(element * 2); // implicit dereference because * expects i32; output.push(*el) would be &i32
     }
 
     output
@@ -24,7 +24,8 @@ fn vec_map(input: &[i32]) -> Vec<i32> {
     input
         .iter()
         .map(|element| {
-            // ???
+            element * 2
+            // return element * 2;
         })
         .collect()
 }
